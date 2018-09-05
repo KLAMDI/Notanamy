1d7d9506bd503aa13527c3bdb71562455df40717
KLAMDI <crunch@hetnet.nl>
2018-09-05 21:14:21 +0200

    Grappling hook now stops completely on terrain collision

---
+++ Notanamy/Assets/Scripts/GrapplingHook.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrapplingHook : MonoBehaviour {
+
+    private Rigidbody grapplingBody;
+
+    void Start()
+    {
+        grapplingBody = gameObject.GetComponent<Rigidbody>();
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "Terrain") {
+            grapplingBody.constraints = RigidbodyConstraints.FreezePosition;
+        }
+    }
+}
