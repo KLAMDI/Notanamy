8269fc275181e9775ab086fabd490e20dd57cdb4
TyroRNG <Velema98@gmail.com>
2018-09-12 22:07:05 +0200

    Beginning attack scripts
    Made a hitbox
    Attack scripts
    Hitbox scrips

---
+++ Notanamy/Assets/Scripts/Attacking/Hitbox.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hitbox : MonoBehaviour {
+
+    public int attackTimer;
+    public bool isScytheSpin = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        attackTimer--;
+        if(attackTimer <= 0)
+        {
+            Destroy(gameObject);
+        }
+	}
+}
