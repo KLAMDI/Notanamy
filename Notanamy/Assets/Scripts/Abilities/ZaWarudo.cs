458d83594ad92f6535a66ca9153309cc3e993702
KLAMDI <crunch@hetnet.nl>
2018-10-31 21:26:32 +0100

    Time slow now works as intended (except for some errors)

--- Notanamy/Assets/Scripts/Abilities/ZaWarudo.cs
+++ Notanamy/Assets/Scripts/Abilities/ZaWarudo.cs
@@ -5,23 +5,30 @@ using UnityEngine;
 public class ZaWarudo : MonoBehaviour {
 
     public float timeSlowStrength;
+    public List<GameObject> slowedObjects = new List<GameObject>();
 
 	// Use this for initialization
 	void Start () {
-		
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+
+        foreach (var item in slowedObjects)
+        {
+            item.GetComponent<Rigidbody>().AddForce(-Physics.gravity*(1 - timeSlowStrength) * item.GetComponent<Rigidbody>().mass);
+        }
+    }
 
     void OnTriggerEnter(Collider other)
     {
         if (other.transform.tag != "Player")
         {
             other.gameObject.GetComponent<Rigidbody>().velocity = other.gameObject.GetComponent<Rigidbody>().velocity*timeSlowStrength;
-            other.gameObject.GetComponent<Rigidbody>().mass = other.gameObject.GetComponent<Rigidbody>().mass * timeSlowStrength;
+            other.gameObject.GetComponent<Rigidbody>().mass = other.gameObject.GetComponent<Rigidbody>().mass / timeSlowStrength;
+
+            slowedObjects.Add(other.gameObject);
         }
     }
 
@@ -30,7 +37,9 @@ public class ZaWarudo : MonoBehaviour {
         if (other.transform.tag != "Player")
         {
             other.gameObject.GetComponent<Rigidbody>().velocity = other.gameObject.GetComponent<Rigidbody>().velocity / timeSlowStrength;
-            other.gameObject.GetComponent<Rigidbody>().mass = other.gameObject.GetComponent<Rigidbody>().mass / timeSlowStrength;
+            other.gameObject.GetComponent<Rigidbody>().mass = other.gameObject.GetComponent<Rigidbody>().mass * timeSlowStrength;
+
+            slowedObjects.Remove(other.gameObject);
         }
     }
 }
