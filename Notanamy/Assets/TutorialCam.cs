76a6991d0e175ad93710e0027f4752498bfa20a4
TyroRNG <Velema98@gmail.com>
2019-01-08 20:21:08 +0100

    Creation of tutorial base

--- Notanamy/Assets/TutorialCam.cs
+++ Notanamy/Assets/TutorialCam.cs
@@ -4,14 +4,26 @@ using UnityEngine;
 
 public class TutorialCam : MonoBehaviour {
 
+    //The player to which the camera is bound
     GameObject pl;
 
+    //Let the player move without movining the camera
     public float maxOfsetX;
     public float maxOfsetY;
 
+    //The calculated next coordinates of the camera
     float nextX;
     float nextY;
 
+    //Select if the camera is bound between coordinates
+    public bool xBound;
+    public bool yBound;
+
+    //The values inbetween which the camera is bound
+    public float minX;
+    public float maxX;
+    public float minY;
+    public float maxY;
 
 	// Use this for initialization
 	void Start () {
@@ -21,6 +33,8 @@ public class TutorialCam : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
+
+        //Make the camera follow the player with the given ofset
         if (pl.transform.position.x > transform.position.x + maxOfsetX)
         {
             nextX = pl.transform.position.x - maxOfsetX;
@@ -41,18 +55,32 @@ public class TutorialCam : MonoBehaviour {
             nextY = pl.transform.position.y + maxOfsetY;
         }
 
-
-
-
-
-        if (nextX < 0)
+        //Keep the x position inbetween the given coordinates when on
+        if (xBound)
         {
-            nextX = 0;
+            if (nextX < minX)
+            {
+                nextX = minX;
+            }
+            if (nextX > maxX)
+            {
+                nextX = maxX;
+            }
         }
-        if (nextY < 1)
+        //Keep the y position inbetween the given coordinates when on
+        if (yBound)
         {
-            nextY = 1;
+            if (nextY < minY)
+            {
+                nextY = minY;
+            }
+            if (nextY > maxY)
+            {
+                nextY = maxY;
+            }
         }
+
+        //Apply the calculated next coordinates
         transform.position = new Vector3(nextX, nextY, -10);
     }
 }
