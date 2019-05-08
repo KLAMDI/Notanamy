442596e17338f962bfd8b49f55107d60083afcd9
KLAMDI <crunch@hetnet.nl>
2019-05-08 22:04:40 +0200

    Made a script for rotating the planet for level 1

---
+++ Notanamy/Assets/planetRotate.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class planetRotate : MonoBehaviour {
+
+    public float rotateSpeed;
+    public GameObject player;
+
+    private GameObject planet;
+    private Movement movement;
+    private float playerVelocity;
+
+    // Use this for initialization
+    void Start () {
+        planet = GameObject.Find("Planet");
+        movement = player.GetComponent<Movement>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        playerVelocity = movement.movementSpeed*rotateSpeed;
+        player.transform.position = new Vector3(0, player.transform.position.y, player.transform.position.z);
+        planet.transform.Rotate(0, 0, playerVelocity);
+
+    }
+}
